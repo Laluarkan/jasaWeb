43753764e9fef8cbd427db4a5246c3874a112f23
Laluarkan <lalunaufalarkan21@gmail.com>
2025-11-11 23:35:29 +0800

    Final: Menggunakan CloudinaryField dan migrasi baru

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.urls import reverse
+from cloudinary.models import CloudinaryField
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     # Pastikan pip install Pillow
-    image = models.ImageField(upload_to='projects/') 
+    image = CloudinaryField(
+        'image',
+        folder='projects', # Ini akan membuat folder 'projects' di Cloudinary
+        resource_type='image',
+        null=True,
+        blank=True
+    ) 
     url = models.URLField(blank=True, null=True, help_text="Link ke website live (opsional)")
     date_completed = models.DateField()
 
@@ -18,3 +25,8 @@ class Project(models.Model):
     def get_absolute_url(self):
         # Ini membuat URL untuk detail proyek, misal: /portfolio/3/
         return reverse('portfolio:project_detail', kwargs={'pk': self.pk})
+
+    def image_url(self):
+        if self.image:
+            return self.image.url
+        return '#' # Atau URL gambar placeholder
